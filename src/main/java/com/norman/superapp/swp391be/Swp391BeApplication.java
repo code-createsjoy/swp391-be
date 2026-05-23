7ea30f1820fb9f4ba46528e13c1fe0fb60fa66db
normanqt2005 <quocthaiarct@gmail.com>
2026-05-23 08:58:27 +0700

    1st upload

---
+++ src/main/java/com/norman/superapp/swp391be/Swp391BeApplication.java
@@ -0,0 +1,13 @@
+package com.norman.superapp.swp391be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp391BeApplication.class, args);
+    }
+
+}
